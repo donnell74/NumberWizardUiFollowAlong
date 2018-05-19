f7e2bf9d5f1e3563c2c27824fe380242836d141a
Greg Donnell <gregory.donnell74@gmail.com>
2018-05-18 18:09:12 -0600

    End of S4 L49

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string levelName) {
+        Debug.Log("Level load requested for:" + levelName);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quiting");
+    }
+}
