065f39831fb28f1a3f628f7fb69981740827bce1
Greg Donnell <gregory.donnell74@gmail.com>
2018-05-19 06:48:43 -0600

    End of S4 L51

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
     public void LoadLevel(string levelName) {
         Debug.Log("Level load requested for:" + levelName);
+        SceneManager.LoadScene(levelName);
     }
 
     public void QuitRequest() {
         Debug.Log("Quiting");
+        Application.Quit();
     }
 }
